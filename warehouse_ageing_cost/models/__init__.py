39cbae926a7a96ecb23f4140c4dd600445ed2f9b
Amro <amro@odooerp.ae>
2026-03-16 10:46:15 +0400

    fix

--- warehouse_ageing_cost/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import product_category
-from . import product_product
-from . import stock_move
-from . import stock_warehouse
--- warehouse_ageing_cost/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import test_product_category
-from . import test_ageing_cost_calculation
-from . import test_ageing_reports
